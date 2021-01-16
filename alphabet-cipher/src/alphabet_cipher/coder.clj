79363beec8a7e4bd6d4d51e34f626f1cf54d4c2d
thsousa <thaislisousa@gmail.com>
2021-01-16 19:46:45 +0000

    alphabet cipher almost there

--- alphabet-cipher/src/alphabet_cipher/coder.clj
+++ alphabet-cipher/src/alphabet_cipher/coder.clj
@@ -1,11 +1,38 @@
 (ns alphabet-cipher.coder)
 
+(def alphabet-base (int \a))
+(def alphabet-length (+ 1 (- (int \z) (int \a))))
+
+(defn char->int [c]
+  (- (int c) alphabet-base))
+
+(defn int->char [i]
+  (char (+ alphabet-base (mod i alphabet-length))))
+
+(defn extended-keyword [keyword length]
+  (if (< (count keyword) length)
+    (let [extended-keyword (range length)
+          initial-length (count keyword)]
+      (mapv #(nth keyword (mod % initial-length)) extended-keyword))
+    (seq keyword)))
+
+(defn rotate-letter [op c1 c2]
+  (int->char (op (char->int c1) (char->int c2))))
+
+(defn rotate-word [keyword message op]
+  (->> (count message)
+       (extended-keyword keyword)
+       (map #(rotate-letter op %1 %2) (seq message))
+       (apply str)))
+
 (defn encode [keyword message]
-  "encodeme")
+  (rotate-word keyword message +))
 
 (defn decode [keyword message]
-  "decodeme")
+  (rotate-word keyword message -))
 
 (defn decipher [cipher message]
-  "decypherme")
+  (decode message cipher)
+  ; TODO: find the size of the pattern
+  )
 
--- alphabet-cipher/test/alphabet_cipher/coder_test.clj
+++ alphabet-cipher/test/alphabet_cipher/coder_test.clj
@@ -2,12 +2,28 @@
   (:require [clojure.test :refer :all]
             [alphabet-cipher.coder :refer :all]))
 
+(deftest test-extended-keyword
+  (testing "extend a keyword by repeating its chars"
+    (is (= [\a \b \c \a \b]
+           (extended-keyword "abc" 5)))))
+
+(deftest test-int->char
+  (testing "can transform an int into a char"
+    (is (= \a (int->char 0)))
+    (is (= \z (int->char 25)))))
+
+(deftest test-rotate
+  (testing "rotate a letter"
+    (is (= \b (rotate-letter + \a \b)))
+    (is (= \z (rotate-letter - \a \b)))))
+
 (deftest test-encode
   (testing "can encode a message with a secret keyword"
     (is (= "hmkbxebpxpmyllyrxiiqtoltfgzzv"
            (encode "vigilance" "meetmeontuesdayeveningatseven")))
     (is (= "egsgqwtahuiljgs"
            (encode "scones" "meetmebythetree")))))
+
 
 (deftest test-decode
   (testing "can decode a message given an encoded message and a secret keyword"
